61658b5f0fbab0bbe3dd4abd0b5dbe70152f8241
Stanislaw Baranski <stan@stan.bar>
2021-07-04 13:35:43 +0200

    update fish variables

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,40 +1,42 @@
 # vim: set ft=fish:
 
-set -U fish_color_normal normal
-set -U fish_color_command 99cc99
-set -U fish_color_quote ffcc66
-set -U fish_color_redirection d3d0c8
-set -U fish_color_end cc99cc
-set -U fish_color_error f2777a
-set -U fish_color_param d3d0c8
-set -U fish_color_comment ffcc66
-set -U fish_color_match 6699cc
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator 6699cc
-set -U fish_color_escape 66cccc
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 747369
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel -r
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix white --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
+set -x fish_color_normal normal
+set -x fish_color_command 99cc99
+set -x fish_color_quote ffcc66
+set -x fish_color_redirection d3d0c8
+set -x fish_color_end cc99cc
+set -x fish_color_error f2777a
+set -x fish_color_param d3d0c8
+set -x fish_color_comment ffcc66
+set -x fish_color_match 6699cc
+set -x fish_color_selection white --bold --background=brblack
+set -x fish_color_search_match bryellow --background=brblack
+set -x fish_color_history_current --bold
+set -x fish_color_operator 6699cc
+set -x fish_color_escape 66cccc
+set -x fish_color_cwd green
+set -x fish_color_cwd_root red
+set -x fish_color_valid_path --underline
+set -x fish_color_autosuggestion 747369
+set -x fish_color_user brgreen
+set -x fish_color_host normal
+set -x fish_color_cancel -r
+set -x fish_pager_color_completion normal
+set -x fish_pager_color_description B3A06D yellow
+set -x fish_pager_color_prefix white --bold --underline
+set -x fish_pager_color_progress brwhite --background=cyan
 
-set -U LANG en_US.UTF-8
-set -U LC_ALL en_US.UTF-8
-set -U EDITOR nvim
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+set -x EDITOR nvim
+set -x MARKDOWN_EDITOR Typora
 
 fish_add_path ~/.cargo/bin
-fish_add_path ~/bin
-fish_add_path ~/dotfiles/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
+fish_add_path ~/dotfiles/bin
+fish_add_path ~/bin
+fish_add_path --prepend /usr/local/bin # prepend to be the first on the list
 
 # Disable vim mode indicator
 function fish_mode_prompt
